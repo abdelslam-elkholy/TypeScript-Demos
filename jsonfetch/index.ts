4ffb699c8050db6985fd8e1de611941ca5b9f6d6
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-02-13 19:28:09 +0200

    update

--- jsonfetch/index.ts
+++ jsonfetch/index.ts
@@ -1,7 +1,22 @@
 import axios from "axios";
 
-const url = "https://jsonplaceholder.typicode.com/todos";
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+const url = "https://jsonplaceholder.typicode.com/todos/2";
 
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+    `);
 });
