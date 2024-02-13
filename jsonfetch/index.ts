3a09072cd3d18f9fd9a14b36e739dac9de17c9fc
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-02-13 19:40:22 +0200

    update

--- jsonfetch/index.ts
+++ jsonfetch/index.ts
@@ -14,9 +14,13 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const finished = todo.completed;
 
+  logTodo(ID, title, finished);
+});
+
+const logTodo = (id: number, title: string, finished: boolean) => {
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${finished}
     `);
-});
+};
