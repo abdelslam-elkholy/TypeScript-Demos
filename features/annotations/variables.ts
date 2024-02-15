74e92044d15da2a6caa8042edb16d906742abd5e
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-02-15 21:57:04 +0200

    update

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,2 +1,10 @@
 const apples: number = 5;
 const speed: string = "sss";
+
+let colors: string[] = ["red", "green", "yellow"];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = { x: 0, y: 0 };
