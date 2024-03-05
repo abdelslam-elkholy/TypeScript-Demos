19c363e8d66e086263076c96bbe4e8ebb242277f
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-03-05 23:13:35 +0200

    update

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -2,4 +2,15 @@ class Vehicle {
   drive(): void {
     console.log("chugaa chugaa");
   }
+
+  honk(): void {
+    console.log("beeb");
+  }
 }
+
+class Car extends Vehicle {}
+
+const car = new Car();
+
+car.drive();
+car.honk();
