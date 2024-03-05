131a7e8d712a20eddea42cd55d8db6131fe4f9e8
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-03-06 01:42:41 +0200

    init new ts tuturial

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -7,5 +7,11 @@ class User {
     lng: number;
   };
 
-  constructor(name: string, location: { lat: number; lng: number }) {}
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
