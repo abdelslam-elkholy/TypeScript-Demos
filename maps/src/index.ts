859a2ae689dca7a51257ee82db467889f979bf92
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-03-06 21:50:57 +0200

    update

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,8 @@
+/// <reference types="@types/google.maps" />
 import { Company } from "./Company";
 import { User } from "./User";
 
 const user = new User();
 const company = new Company();
-
+google.maps;
 console.log(user);
