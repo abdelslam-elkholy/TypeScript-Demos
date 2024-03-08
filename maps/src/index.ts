556929c89f82accb7e67a910d7a5758306679f56
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-03-09 01:32:44 +0200

    update

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,16 @@
 /// <reference types="@types/google.maps" />
-import { Company } from "./Company";
-import { User } from "./User";
+// import { Company } from "./Company";
+// // import { User } from "./User";
 
-const user = new User();
-const company = new Company();
-google.maps;
-console.log(user);
+// const user = new User();
+// const company = new Company();
+// google.maps;
+// console.log(user);
+
+new google.maps.Map(document.getElementById("map")!, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
