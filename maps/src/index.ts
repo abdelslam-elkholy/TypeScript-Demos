e6b31bd74ca26f2da69bfbb34573b2f78428ea12
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-03-09 03:01:29 +0200

    update

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,11 +1,10 @@
 import { Company } from "./Company";
 import { User } from "./User";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
 const company = new Company();
-// const company = new Company();
-// google.maps;
-// console.log(user);
-import { CustomMap } from "./CustomMap";
+
 const customMap = new CustomMap();
 customMap.addMarker(user);
+customMap.addMarker(company);
